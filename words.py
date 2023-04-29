c5e0932ba422942ebeb60b444dd7f60d1aa55918
Roman Kitsela <roman.kitsela@gmail.com>
2023-04-29 18:22:23 +0100

    refactor code

--- words.py
+++ words.py
@@ -8,9 +8,15 @@ def fetch_words():
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
 
+def print_words(story_words):
     for word in story_words:
         print(word)
+
+def main():
+    words = fetch_words()
+    print_words(words)
 
 """
 This controls when the above function is executed.
@@ -18,4 +24,4 @@ When words.py is run as a script from the terminal --> fetch_words() is called
 When the words module is imported --> the function above is defined but the function is not called
 """
 if __name__ == '__main__':
-    fetch_words()
+    main()
